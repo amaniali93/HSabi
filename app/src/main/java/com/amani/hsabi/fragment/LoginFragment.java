2969bab2c65d357218d72241fafd6ad933acbc66
Yousuf <josephali1993@gmail.com>
2020-06-09 15:03:27 +0400

    #fix LoginFragment, RegisterFragment issue.

--- app/src/main/java/com/amani/hsabi/fragment/LoginFragment.java
+++ app/src/main/java/com/amani/hsabi/fragment/LoginFragment.java
@@ -2,24 +2,17 @@ package com.amani.hsabi.fragment;
 
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Color;
-import android.graphics.PorterDuff;
-import android.graphics.drawable.Drawable;
-import android.os.Build;
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.core.content.ContextCompat;
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
 
 import com.amani.hsabi.Activites.FunctionActivity;
 import com.amani.hsabi.Interfaces.MediaInterface;
@@ -30,11 +23,7 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link LoginFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class LoginFragment extends Fragment {
     EditText emailid, password;
     Button btnlogin;
@@ -47,6 +36,13 @@ public class LoginFragment extends Fragment {
     }
 
     private MediaInterface mListener;
+
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+    }
 
     @Override
     public void onAttach(Context context) {
@@ -106,27 +102,30 @@ public class LoginFragment extends Fragment {
                                         startActivity(intefunction);
                                     }
 
-
                                 }
 
                             });
-                    TextView btnNext = parentView.findViewById(R.id.register);
-                    btnNext.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View view) {
-                            if (mListener != null) {
-                                mListener.changeFragmentTo(new RegisterFragment(), RegisterFragment.class.getSimpleName());
-                            }
-                        }
-                    });
-
-                    @Override
-                    protected Void onStart () {
-                        super.onStart();
-                        mFirebaseAuth.addAuthStateListener(aAuthstatelistener);
-                    }
                 } else {
                     Toast.makeText(getActivity(), "Error ocurred!", Toast.LENGTH_SHORT).show();
                 }
 
             }
+        });
+
+        TextView btnNext = parentView.findViewById(R.id.register);
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mListener != null) {
+                    mListener.changeFragmentTo(new RegisterFragment(), RegisterFragment.class.getSimpleName());
+                }
+            }
+        });
+
+
+        return parentView;
+    }
+
+}
+
+
--- app/src/main/java/com/amani/hsabi/fragment/RegisterFragment.java
+++ app/src/main/java/com/amani/hsabi/fragment/RegisterFragment.java
@@ -3,10 +3,6 @@ package com.amani.hsabi.fragment;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,6 +10,9 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
 
 import com.amani.hsabi.Activites.FunctionActivity;
 import com.amani.hsabi.Interfaces.MediaInterface;
@@ -23,13 +22,9 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link RegisterFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class RegisterFragment extends Fragment {
-    EditText emailid ,password,phoneNo;
+    EditText emailid, password, phoneNo;
     Button btnregister;
     FirebaseAuth mFirebaseAuth;
 
@@ -67,44 +62,40 @@ public class RegisterFragment extends Fragment {
         btnregister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String email= emailid.getText().toString();
+                String email = emailid.getText().toString();
                 String passWord = password.getText().toString();
                 String mobileNo = phoneNo.getText().toString();
-                if(email.isEmpty()){
+                if (email.isEmpty()) {
                     emailid.setError("Please enter emailId ");
                     emailid.requestFocus();
-                }
-                else if (passWord.isEmpty()){
+                } else if (passWord.isEmpty()) {
                     password.setError("Please enter password");
                     password.requestFocus();
-                }
-                else if (mobileNo.isEmpty()){
+                } else if (mobileNo.isEmpty()) {
                     phoneNo.setError("Please enter Phone Number");
                     phoneNo.requestFocus();
-                }
-                else if (email.isEmpty() && passWord.isEmpty() && mobileNo.isEmpty()){
+                } else if (email.isEmpty() && passWord.isEmpty() && mobileNo.isEmpty()) {
                     Toast.makeText(getActivity(), "Fields are Empty!", Toast.LENGTH_SHORT).show();
-                }
-                else if (!(email.isEmpty() && passWord.isEmpty())){
-                    mFirebaseAuth.createUserWithEmailAndPassword(email , passWord).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                        @Override
-                        public void onComplete(@NonNull Task<AuthResult> task) {
-                            if(!task.isSuccessful()){
-                                Toast.makeText(getActivity(), "Register UnSuccessful, Please try again!", Toast.LENGTH_SHORT).show();
+                } else if (!(email.isEmpty() && passWord.isEmpty())) {
+                    mFirebaseAuth.createUserWithEmailAndPassword(email, passWord)
+                            .addOnCompleteListener(getActivity(), new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (!task.isSuccessful()) {
+                                        Toast.makeText(getActivity(), "Register UnSuccessful, Please try again!", Toast.LENGTH_SHORT).show();
 
-                            }
-                            else {
-                                Intent intent = new Intent(getActivity().getApplication(), FunctionActivity.class);
-                                startActivity(intent);
-                            }
-                        }
-                    }
-                }
-                else {
+                                    } else {
+                                        Intent intent = new Intent(getActivity().getApplication(), FunctionActivity.class);
+                                        startActivity(intent);
+                                    }
+                                }
+                            });
+                } else {
                     Toast.makeText(getActivity(), "Error ocurred!", Toast.LENGTH_SHORT).show();
                 }
             }
         });
+
         btnNext.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -113,3 +104,9 @@ public class RegisterFragment extends Fragment {
                 }
             }
         });
+
+        return parentView;
+    }
+
+}
+
